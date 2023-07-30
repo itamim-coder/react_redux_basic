2af2289b4d4a48551d935e4a3b534222dae3b869
itamim-coder <itamim12202@gmail.com>
2023-07-31 02:56:56 +0600

    redux store setup with logger

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/Features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <button
+          className="rounded-full bg-yellow-500"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <span>{count}</span>
+        <button
+          className="rounded-full bg-yellow-500"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          className="rounded-full bg-yellow-500"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment by amount
+        </button>
+      </div>
     </>
   );
 }
